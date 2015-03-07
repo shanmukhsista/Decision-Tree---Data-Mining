81e1f6c0f0b33b8de1f6d8deb51ad69047db00fa
shanmukhsista <shanmukh@outlook.com>
2015-03-07 09:17:14 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/homework3/Record.java

--- src/homework3/AttributeValue.java
+++ src/homework3/AttributeValue.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AttributeValue {
-	String name; 
+	String count; 
 	String desc ; 
 	public AttributeValue(String name){
 		//Get the list of all distinct attributes from teh database. 	
